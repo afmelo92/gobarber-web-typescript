e817fca0b1676cba74e8f43042bdcfe055ae4432
Andre Melo <andre.fabian.melo@gmail.com>
2020-05-08 22:23:03 -0300

    Add register page

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,13 @@
 import React from 'react';
 
 import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
 import GlobalStyle from './styles/global';
 
 function App() {
   return (
     <>
-      <SignIn />
+      <SignUp />
       <GlobalStyle />
     </>
   );
