4b866696b2ca5ddd8b1f17e093b78d675402ae33
Andre Melo <andre.fabian.melo@gmail.com>
2020-05-08 22:05:44 -0300

    Add auth page

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+import React from 'react';
+
+import SignIn from './pages/SignIn';
+import GlobalStyle from './styles/global';
+
+function App() {
+  return (
+    <>
+      <SignIn />
+      <GlobalStyle />
+    </>
+  );
+}
+
+export default App;
